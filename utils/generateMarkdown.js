864070d370cdffd6b691d03687c5e34f2f267eb1
Jared Love <lovejared91@yahoo.com>
2022-09-14 21:06:58 -0500

    added function to generate the markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,1 +1,57 @@
 
+function generateMarkdown(input) {
+
+    let markDown = 
+    `  
+    
+    ## ${input.ptitle}
+    
+    
+    ## Name
+    
+    ${input.name}
+    
+    
+    
+    ## Github
+    
+    ${input.github}
+    
+    
+    
+    ## Description
+    
+    ${input.description}
+    
+    
+    
+    
+    ## Table of Contents
+     
+    
+    ## License
+    
+    ${input.license}
+    
+    
+    ## Usage
+    
+    ${input.usage}
+    
+    
+    ## Credits
+    
+    ${input.credits}
+    
+    
+    
+    ## How to Contribute
+    
+    ${contribute}
+    `
+    return markDown;
+    }
+    
+    
+    
+    module.exports = generateMarkdown;
